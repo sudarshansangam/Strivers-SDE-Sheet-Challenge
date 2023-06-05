dc277fb721b4d170e7de300ebedd9717f7d75829
sudarshan-hub <sudarshan120305@gmail.com>
2023-06-05 12:18:41 +0530

    Search in 2D Array

--- Search in a 2D Array.py
+++ Search in a 2D Array.py
@@ -1,6 +1,18 @@
+#  You have been given a 2-D array "MAT" of size M X N where 'M'
+#  and 'N' denote the number of rows and columns, respectively. The
+#  elements of each row are sorted in non-decreasing order.
+#  Moreover, the first element of a row is greater than the last
+#  element of the previous row (if it exists).
 
+#  You are given an integer 'TARGET,' and your task is to find if it
+#  exists in the given 'MAT' or not.
 
-Solution:
+#  Example:
+#    Input: 'M' = 3, 'N' = 4, 'mat' = [[1, 2, 3, 4], [5,
+#    6, 7, 8], [9, 10, 11, 12]], 'target' = 8
+#    Output:
+
+# Solution:
 
 def searchMatrix(mat: [[int]], target: int) -> bool:
     # Write your code here.
