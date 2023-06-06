5adb1846f75059163960b9b24f39b7c3d27412be
sudarshan-hub <sudarshan120305@gmail.com>
2023-06-06 12:55:37 +0530

    Cycle detection II

---
+++ Cycle detection II.cpp
@@ -0,0 +1,46 @@
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+// Do not modify the linked list.
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+
+// SOLUTION:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head, *enter = head;
+        while((fast != NULL) && (fast->next != NULL)){
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast){
+                while(enter != slow){
+                    enter = enter->next;
+                    slow = slow->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
